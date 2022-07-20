1d9b3ab440d9361a6f4284e871e522aae59d4135
m1994d <marlondiaz2994@gmail.com>
2022-07-20 14:24:48 -0500

    proofreading

---
+++ converter.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * converter - function to convert string
+ * @num: parameter nnumber
+ * @base: parameter base
+ * @upp: uppercase
+ * Return: char*
+ */
+char *converter(unsigned long int num, int base, bool upp)
+{
+	static char *r;
+	static char buffer[50];
+	char *ptr;
+
+	if (upp)
+	{
+		r = "0123456789ABCDEF";
+	}
+	else
+	{
+		r = "0123456789abcdef";
+	}
+		
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = r[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (ptr);
+}
