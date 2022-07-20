fd33c43eb3c0321f225c70839f4fa624de4ba215
m1994d <marlondiaz2994@gmail.com>
2022-07-20 13:52:29 -0500

    get_fun_otspecif.c

---
+++ get_fun_otspecif.c
@@ -0,0 +1,92 @@
+#include "main.h"
+/**
+ * print_undec - prints unsigned decimal of a unsigned int
+ * @arg: integer pass by user
+ * Return: characters printed
+ */
+int print_undec(va_list arg)
+{
+	unsigned int num;
+	char *str;
+	int i = 0;
+
+	num = va_arg(arg, unsigned int);
+	str = converter(num, 10, false);
+	if (str == NULL)
+		return (0);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_oct - prints the number octal of a unsigned int
+ * @arg: integer pass by user
+ * Return: characters printed
+ */
+int print_oct(va_list arg)
+{
+	unsigned int num;
+	char *str;
+	int i = 0;
+
+	num = va_arg(arg, unsigned int);
+	str = converter(num, 8, false);
+	if (str == NULL)
+		return (0);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_hex - prints hexadecimal of a unsigned int
+ * @arg: integer pass by user
+ * Return: characters printed
+ */
+int print_hex(va_list arg)
+{
+	unsigned int num;
+	char *str;
+	int i = 0;
+
+	num = va_arg(arg, unsigned int);
+	str = converter(num, 16, false);
+	if (str == NULL)
+		return (0);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_HEX - prints HEXadecimal of a unsigned int
+ * @arg: integer pass by user
+ * Return: characters printed
+ */
+int print_HEX(va_list arg)
+{
+	unsigned int num;
+	char *str;
+	int i = 0;
+
+	num = va_arg(arg, unsigned int);
+	str = converter(num, 16, true);
+	if (str == NULL)
+		return (0);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
