fce35133eeb3068bf4af2be5a73f5e15acb6fdb2
R-andyr <tyraeljacob@gmail.com>
2022-07-20 14:01:41 -0700

    compilation and proofreading

--- _printf.c
+++ _printf.c
@@ -20,21 +20,21 @@ int _printf(const char *format, ...)
 		if (format[a] == '%')
 		{
 			a++;
-			if (format[a] != '%' && format[j] != 'd'
-			&& format[a] != 'i' && format[j] != 'c'
-			&& format[a] != 's' && format[j] != 'b'
-			&& format[a] != 'u' && format[j] != 'o'
-			&& format[a] != 'x' && format[j] != 'X')
+			if (format[a] != '%' && format[a] != 'd'
+			&& format[a] != 'i' && format[a] != 'c'
+			&& format[a] != 's' && format[a] != 'b'
+			&& format[a] != 'u' && format[a] != 'o'
+			&& format[a] != 'x' && format[a] != 'X')
 			{
 				if (format[a - 1] == '%' && format[a] == '\0')
 					return (-1);
 
-				i = i + _putchar(format[j - 1]);
-				i = i + _putchar(format[j]);
+				i = i + _putchar(format[a - 1]);
+				i = i + _putchar(format[a]);
 			}
 			else
 			{
-				f = get_func(&format[j]);
+				f = get_func(&format[a]);
 				i = i + f(arg);
 			}
 		}
