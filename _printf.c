1d86c67c7d88babcb73cd37f0e8f02ae662ae465
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-07-22 14:04:33 -0500

    ldsad

--- _printf.c
+++ _printf.c
@@ -9,7 +9,8 @@
 int _printf(const char *format, ...)
 {
     va_list va_printf;
-    int buf = 0, ch = 0;
+    int buf = 0, ch = 0, j = 0;
+    char letters_for_functions[4] = {'d', 'i', 'c', 's'};
 
     if (!format || !*format)
         return (-1);
@@ -20,16 +21,19 @@ int _printf(const char *format, ...)
     {
         if (format[ch] == '%')
         {
+            for(j = 0; letters_for_functions[j]; j++)
+            {
+                if (format[ch + 1] == letters_for_functions[j])
+                {
+                    ch++;
+                    buf += get_function(&format[ch])(va_printf);
+                    ch++;
+                }
+            }
+
             if (format[ch + 1] == '%')
             {
                 _printchar(format[ch]);
-                ch++;
-            }
-
-            else if (format[ch + 1] == 'c' || format[ch + 1] == 's'
-            || format[ch + 1] == 'd' || format[ch + 1] == 'i')
-            {
-                buf += get_function(&format[ch + 1])(va_printf);
                 ch++;
             }
 
